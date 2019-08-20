43184fe2ec4b8b76208bfc89b27b00c444786927
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-20 13:47:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,18 @@
-# def reverse_each_word(string)
-#   reversed = []
-#   string.split.collect do | word |
-#     reversed << word.reverse
-#   end
-#   puts reversed.join(" ")
-#   reversed.join(" ")
-# end
-
-
 def reverse_each_word(string)
   reversed = []
-  string.split.each do | word |
+  string.split.collect do | word |
     reversed << word.reverse
   end
   puts reversed.join(" ")
   reversed.join(" ")
 end
+
+
+# def reverse_each_word(string)
+#   reversed = []
+#   string.split.each do | word |
+#     reversed << word.reverse
+#   end
+#   puts reversed.join(" ")
+#   reversed.join(" ")
+# end
