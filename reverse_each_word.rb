bb7a96cf9409f86450f9bbdd523879539bb70ece
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-20 13:44:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,12 @@ def reverse_each_word(string)
   end
   reversed.join(" ")
 end
+
+
+# def reverse_each_word(string)
+#   reversed = []
+#   string.split.each do | word |
+#     reversed << word.reverse
+#   end
+#   reversed.join(" ")
+# end
